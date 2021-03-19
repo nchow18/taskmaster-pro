58039201719316bc0266f442fdf9700bcb2363d0
Nathan Chow <emailme@nathanchow.ca>
2021-03-19 14:01:59 -0400

    update

---
+++ Note-Taker/README.md
@@ -0,0 +1,63 @@
+
+![Badge](https://img.shields.io/badge/AWESOME-COOL-yellow.svg)
+
+# PROJECT TITLE: Note Taker
+
+* [Link to GitHub](https://github.com/nchow18)
+* [Email]:(mailto:emailme@nathanchow.ca)
+
+## DESCRIPTION:
+
+This is a site that stores and displays your current and past notes
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Badges](#badges)
+* [Contributing](#contribute)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+To install: add node.js to start the server for server.js.  After it has been installed, go to the website http://localhost:3001 and begin the note taking!
+
+## Usage
+
+to be used by anyone
+
+<<<<<<< HEAD
+* ![Instructional Video](https://youtu.be/xBRlVX3DLT8)
+=======
+[![Instructional Video On YOUTUBE](https://img.youtube.com/vi/VIDEO-ID/0.jpg)](https://youtu.be/xBRlVX3DLT8)
+>>>>>>> 769bf2909b2288804587f7696c21dc7f3dd4277a
+
+## Credits
+
+nchow18
+
+## License
+
+Apache
+
+
+## Badges
+
+* ![Badge](https://img.shields.io/badge/AWESOME-COOL-yellow.svg)
+
+## Contributing
+
+input with any code and errors
+
+## Tests
+
+to test, begin npm start in command line to start the node.js server and active localhost:3001
+
+## Questions
+
+
+* none
+
+    
+
+    
---
+++ Note-Taker/server.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// grabs data from public folder: index.html/notes.html
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());heroku
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+})
+
+const { data } = require('./db/db');
+
+console.table(data);
+
+// to valide the data being saved to the note
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
+
+// creating new object to notes array
+function createNewNote(body, dataArray) {
+    const note = body;
+    dataArray.push(note);
+    //writing the data to db.json
+    fs.writeFileSync(
+        //creating new file db.json with new data
+        path.join(__dirname, './db/db.json'),
+        //adding spaces to array
+        JSON.stringify({ data: dataArray }, null, 2)
+    );
+    return note;
+}
+
+function findById(id, dataArray) {
+    const results = dataArray.filter(data => data.id === id)
+    return results;
+}
+
+function deleteNote(id, dataArray) {
+    const results = data;
+    console.log('printing id of deleteNote: ' + id);
+    dataArray.splice(id, 1);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ data: dataArray }, null, 2)
+    )
+    return results;
+}
+
+app.delete('/api/db/:id', (req, res) => {
+    const results = deleteNote(req.params.id, data);
+    if(results) {
+        res.json(results);
+    } else {
+        res.send(404);
+    }
+})
+
+app.get('/api/db', (req, res) => {
+    let results = data;
+    if (req.query) {
+        console.log(results);
+    }
+    res.json(results);
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.post('/api/db', (req, res) => {
+    req.body.id = data.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.') 
+    } else {
+        const note = createNewNote(req.body, data);
+        res.json(note);
+    }
+
+})
+
+
+
