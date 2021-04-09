6196d43541b5ff61d6708157b2220accba93b663
Nathan Chow <emailme@nathanchow.ca>
2021-04-09 17:50:25 -0400

    update files

--- Note-Taker/server.js
+++ Note-Taker/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true}));
-app.use(express.json());heroku
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
